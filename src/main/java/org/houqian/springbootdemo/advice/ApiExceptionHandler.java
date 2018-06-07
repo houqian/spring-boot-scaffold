e42792f1a4ba302324cd64012c3df682972d652c
houqian <houqian1991@finupgroup.com>
2018-06-07 23:58:56 +0800

    add profile config

--- src/main/java/org/houqian/springbootdemo/advice/ApiExceptionHandler.java
+++ src/main/java/org/houqian/springbootdemo/advice/ApiExceptionHandler.java
@@ -1,7 +1,6 @@
 package org.houqian.springbootdemo.advice;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
